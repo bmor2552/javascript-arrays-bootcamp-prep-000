cfda9b5bbfaf116ba9a117cc6809d96985b1677d
Boi Moriba <boi.moriba@gmail.com>
2020-01-19 20:00:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ return array[index]
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift();
+  return array.shift()
 }
 
 function removeElementFromBeginningOfArray(array) {
@@ -30,7 +30,7 @@ return array.slice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-return array.pop();
+return array.pop()
 }
 
 function removeElementFromEndOfArray(array) {
