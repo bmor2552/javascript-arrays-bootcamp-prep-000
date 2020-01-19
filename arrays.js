006c7e8526f6ca3fe30bf3ce46961d8c211dcd24
Boi Moriba <boi.moriba@gmail.com>
2020-01-19 17:58:58 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+function addElementToBeginningOfArray(array, element) {
+    let array.unshift(element)
+    return array
+}
