8e3786cdf80f1278451d89a08efcd9f4a8b39d40
Boi Moriba <boi.moriba@gmail.com>
2020-01-19 18:04:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,5 +5,32 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   return array
 }
 function addElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+  return array.unshift(..element)
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+return array.push(element),
+}
+
+function addElementToEndOfArray(array, element) {
+array.push(element);
+return new array();
+}
+
+function accessElementInArray(array) {
+return array[2];
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift();
+}
+
+function removeElementFromBeginningOfArray(){
+
+}
+
+function destructivelyRemoveElementFromEndOfArray(){
+
+}
+
+function removeElementFromEndOfArray(){
+
 }
